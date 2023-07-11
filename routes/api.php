7f730e8f8c5a8765f057a4fc1612a409ffdbeae1
John Israel Aquino <jbaquino3@up.edu.ph>
2023-07-11 16:23:32 +0800

    generate encounter

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DepartmentController;
 use App\Http\Controllers\PatientController;
+use App\Http\Controllers\EncounterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,10 @@ use App\Http\Controllers\PatientController;
 
 Route::get('/departments', [DepartmentController::class, 'index']);
 Route::get('/patients', [PatientController::class, 'index']);
+
+// group by prefix, controller
+Route::prefix('/encounters')->controller(EncounterController::class)->group(function () {
+    Route::get('/', 'index'); // get /encounters
+    Route::post('/', 'store'); // post /encounters
+});
+
